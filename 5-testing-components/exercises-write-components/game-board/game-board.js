2249b9164fbbce046a2eb4ce8112ca78dc97e39f
AzimaKai <azikikimia@gmail.com>
2022-12-04 15:56:02 +0100

    exercices game board test done

--- 5-testing-components/exercises-write-components/game-board/game-board.js
+++ 5-testing-components/exercises-write-components/game-board/game-board.js
@@ -4,4 +4,17 @@
  * @param {Array[]} arrayOfArrays - a 2D array representing the game board
  * @returns {HTMLTableElement} the rendered game board
  */
-export const gameBoard = (arrayOfArrays) => {};
+export const gameBoard = (arrayOfArrays) => {
+  const tableE1 = document.createElement('table');
+  for (const array of arrayOfArrays) {
+    const trE1 = document.createElement('tr');
+
+    for (const text of array) {
+      const tdE1 = document.createElement('td');
+      tdE1.innerText = text;
+      trE1.appendChild(tdE1);
+    }
+    tableE1.appendChild(trE1);
+  }
+  return tableE1;
+};
